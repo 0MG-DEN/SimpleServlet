ef4484b325589bf083c62c069e432ed7ce00a59a
unknown <loooool2312@gmail.com>
2024-09-01 06:46:11 +0900

    Add default and base class.

---
+++ src/simpleservlet/Default.java
@@ -0,0 +1,27 @@
+package simpleservlet;
+
+import java.io.*;
+import java.nio.file.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Default extends SimpleServlet {
+	private static final long serialVersionUID = 1L;
+
+	private void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String path = Paths.get("WEB-INF", "jsp", "result.jsp").toString();
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		handleRequest(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		handleRequest(request, response);
+	}
+}
--- src/simpleservlet/SimpleServlet.java
+++ src/simpleservlet/SimpleServlet.java
@@ -1,27 +1,12 @@
 package simpleservlet;
 
-import java.io.*;
-import java.nio.file.*;
-
-import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	private void handleRequest(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String path = Paths.get("WEB-INF", "jsp", "result.jsp").toString();
-		request.getRequestDispatcher(path).forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		handleRequest(request, response);
-	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		handleRequest(request, response);
+	protected void log(String format, Object... args) {
+		String message = String.format(format, args);
+		super.log(message);
 	}
 }
